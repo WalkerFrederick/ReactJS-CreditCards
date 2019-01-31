97d8ca8804b67de28f61ebdede6df7a4860221c0
walker <walker@urdev.xyz>
2019-01-30 20:17:35 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CreditCard from './lib/CreditCard/CreditCard'
+
+import './index.css'
+
+
+
+ReactDOM.render(
+
+
+<div className="body">
+    <CreditCard/>
+</div>
+
+, document.getElementById('root'));
---
+++ src/lib/CreditCard/CreditCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './CreditCard.css'
+
+class CreditCard extends React.Component{
+    constructor(props){
+        super(props);
+
+    };
+
+    render(){
+        return (
+            <div className="CREDIT-CARD"></div>
+        );
+    };
+
+}
+
+export default CreditCard;
