947a7b0956f009ba5ef02625c3ee83b86042511c
walker <walker@urdev.xyz>
2019-01-30 20:49:28 -0500

    added function to split credit card number into 4 chunks

--- src/lib/CreditCard/CreditCard.js
+++ src/lib/CreditCard/CreditCard.js
@@ -7,9 +7,21 @@ class CreditCard extends React.Component{
 
     };
 
+    deconstructNumber(number) {
+        let output = '';
+        for(let i = 0; i < number.length/4; i++){
+            output += number.substring(i*4,i*4+4)  + ' ';
+        }
+        return(<span>{output}</span>);
+    }
+
     render(){
         return (
-            <div className="CREDIT-CARD"></div>
+            <div className="CREDIT-CARD">
+                <div className="CREDIT-CARD-NUMBER">{this.deconstructNumber('0123456789876543')}</div>
+                <div className="CREDIT-CARD-NUMBER"></div>
+                <div className="CREDIT-CARD-NUMBER"></div>
+            </div>
         );
     };
 
